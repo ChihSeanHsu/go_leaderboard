74f28659f061b0b94f651e00b3488a07d7ba7b77
Sean C. Hsu <sean_c_hsu@trendmicro.com>
2021-06-08 01:10:23 +0800

    feat: migration.go

--- cmd/migration/migration.go
+++ cmd/migration/migration.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"example.com/leaderboard/pkg/model"
 	"example.com/leaderboard/pkg/repository"
 )
 
 func main() {
 	db := repository.Init(1, 1)
-	db.AutoMigrate(&model.Score{})
+	db.AutoMigrate(&repository.ScoreORM{})
 }
